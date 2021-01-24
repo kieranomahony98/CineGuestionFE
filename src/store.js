d028c4257264c67725530ee621d607b564bb4cb3
kieranomahony <117429464@umail.ucc.ie>
2021-01-24 23:37:08 +0000

    forgot to take away dev tools

--- src/store.js
+++ src/store.js
@@ -16,8 +16,7 @@ const persistedReducer = persistReducer(persistConfig, combineReducers);
 export const store = createStore(
     persistedReducer,
     initialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(applyMiddleware(...middleware)
     ));
 export const persistor = persistStore(store);
 
